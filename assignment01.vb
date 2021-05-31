2288dae6ac318614669253bf7d03806239776eb4
Apeksha <84757520+Apeksha0719@users.noreply.github.com>
2021-05-31 17:34:11 +0530

    Add files via upload

---
+++ assignment01.vb
@@ -0,0 +1,9 @@
+create database person (Membership_id int(200),full_name varchar(200),Physical_address varchar(200),Salutation varchar(200),movies_rented varchar(200))
+insert into person values(1,'janet jones','FirstStreet Plot No 4''2',(Pirates of the carinnean,clash of the titans)),
+                         (2,'Robert Phil','3rd Street 34','1',('Forrgetting Sarah Marshal,Daddys Little Girls))
+						 (3,'Robert Phil,5th Avenue','1',clash of the titans');
+						 
+Select * from person;
+create database person (Membership_id int(200),movies_rented varchar(200),primary key movies_rented varchar(200))
+insert into person values(1,'Pirates of the carinnean'),(1,'clash of the titans')(2,'Forrgetting Sarah Marshal')(2,'Daddys Little Girls')(3,'clash of the titans')
+
